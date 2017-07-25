eaa40324e0ff7b0f6475966b962a4a8df4adcc20
Stephen Schools <stephen.schools>
2017-07-25 13:08:06 -0400

    commit of daily project for intro to javaScript

---
+++ main.js
@@ -0,0 +1,11 @@
+let computer = "paper";
+
+let me = 'scissors';
+
+if (me === "paper") {
+  console.log("Looks like a tie");
+} else if (me === "rock") {
+  console.log("Computer wins!");
+} else if (me === "scissors") {
+  console.log("I win!");
+}
